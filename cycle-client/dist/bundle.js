0b44d54d33be380a1e53ac13c8e28333fd454d5d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-20 17:25:31 -0400

    working listening to created messages via feathers service

--- cycle-client/dist/bundle.js
+++ cycle-client/dist/bundle.js
@@ -52,25 +52,46 @@
 	// const socketio = require('feathers-socketio/client')
 	const io = __webpack_require__(63)
 	const URL = 'http://localhost:3040'
-	// via feathers client
-	const socket = io(URL)
-	const app = feathers()
-	  .configure(feathers.hooks())
-	  .configure(feathers.socketio(socket));
-	const messageService = app.service('messages')
-	window.messageService = messageService
 
-	function main ({DOM}) {
+	function makeMessageDriver(url) {
+	  // via feathers client
+	  const socket = io(URL)
+	  const app = feathers()
+	    .configure(feathers.hooks())
+	    .configure(feathers.socketio(socket));
+	  const messageService = app.service('messages')
+	  // expose on window for simple testing
+	  window.messageService = messageService
+
+	  return function sockDriver(outgoing$) {
+	    outgoing$.subscribe(outgoing => {
+	      console.log('should send outgoing message', outgoing)
+	    })
+
+	    return Rx.Observable.create(observer => {
+	      messageService.on('created', (message) => {
+	        console.log('new message sourced', message)
+	        observer.onNext(message)
+	      })
+	    });
+	  }
+	}
+
+	function main ({DOM, messages}) {
 	  // const incomingMessages$ = socket.get('messages')
 	  // incomingMessages$.map(console.log.bind(console)).subscribe()
 
+	  // messages.do(console.log.bind(console))
+	  messages.subscribe(console.log.bind(console))
+
 	  return {
-	    DOM: Rx.Observable.just(false).map(() => h1('hi'))
+	    DOM: Rx.Observable.just(false).map(() => h1('hi')),
+	    messages: Rx.Observable.just(false)
 	  }
 	}
 	const sources = {
-	  // socket: SocketIO.createSocketIODriver(URL),
-	  DOM: makeDOMDriver('#app')
+	  DOM: makeDOMDriver('#app'),
+	  messages: makeMessageDriver(URL)
 	}
 	Cycle.run(main, sources)
 
--- cycle-client/index.js
+++ cycle-client/index.js
@@ -6,24 +6,45 @@ const {makeDOMDriver, h1} = require('@cycle/dom')
 // const socketio = require('feathers-socketio/client')
 const io = require('socket.io-client')
 const URL = 'http://localhost:3040'
-// via feathers client
-const socket = io(URL)
-const app = feathers()
-  .configure(feathers.hooks())
-  .configure(feathers.socketio(socket));
-const messageService = app.service('messages')
-window.messageService = messageService
 
-function main ({DOM}) {
+function makeMessageDriver(url) {
+  // via feathers client
+  const socket = io(URL)
+  const app = feathers()
+    .configure(feathers.hooks())
+    .configure(feathers.socketio(socket));
+  const messageService = app.service('messages')
+  // expose on window for simple testing
+  window.messageService = messageService
+
+  return function sockDriver(outgoing$) {
+    outgoing$.subscribe(outgoing => {
+      console.log('should send outgoing message', outgoing)
+    })
+
+    return Rx.Observable.create(observer => {
+      messageService.on('created', (message) => {
+        console.log('new message sourced', message)
+        observer.onNext(message)
+      })
+    });
+  }
+}
+
+function main ({DOM, messages}) {
   // const incomingMessages$ = socket.get('messages')
   // incomingMessages$.map(console.log.bind(console)).subscribe()
 
+  // messages.do(console.log.bind(console))
+  messages.subscribe(console.log.bind(console))
+
   return {
-    DOM: Rx.Observable.just(false).map(() => h1('hi'))
+    DOM: Rx.Observable.just(false).map(() => h1('hi')),
+    messages: Rx.Observable.just(false)
   }
 }
 const sources = {
-  // socket: SocketIO.createSocketIODriver(URL),
-  DOM: makeDOMDriver('#app')
+  DOM: makeDOMDriver('#app'),
+  messages: makeMessageDriver(URL)
 }
 Cycle.run(main, sources)
